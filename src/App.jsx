9bd89b98912fef057dba3bf98c3316678ee29d0a
Tony-M45 <tonymousa07@gmail.com>
2026-04-06 18:34:12 -0400

    Added loading, error and no data states

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,72 @@ import SearchBar from "./SearchBar";
 
 function App() {
   const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [searched, setSearched] = useState(false);
 
-  const handleSearch = () => {
-    console.log("Searching for:", city);
-  };
+  const API_KEY = "617eef3e28606cbe3a95d6edbfa6ea3d";
+
+ const handleSearch = async () => {
+  if (city.trim() === "") {
+    setError("Please enter a city name.");
+    setWeather(null);
+    setSearched(false);
+    return;
+  }
+
+  try {
+    setLoading(true);
+    setError("");
+    setSearched(true);
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    );
+
+    const data = await response.json();
+    console.log(data);
+
+    if (!response.ok) {
+      setError(data.message || "City not found.");
+      setWeather(null);
+      return;
+    }
+
+    setWeather(data);
+  } catch (err) {
+    setError("Failed to fetch weather data.");
+    setWeather(null);
+  } finally {
+    setLoading(false);
+  }
+};
 
   return (
-    <div>
-      <h1>React Weather App</h1>
-      <SearchBar city={city} setCity={setCity} onSearch={handleSearch} />
-    </div>
+  <div>
+    <h1>React Weather App</h1>
+
+    <SearchBar city={city} setCity={setCity} onSearch={handleSearch} />
+
+    {loading && <p>Loading...</p>}
+
+    {error && <p>Error: {error}</p>}
+
+    {!loading && !error && searched && !weather && (
+      <p>No data found.</p>
+    )}
+
+    {weather && weather.main && (
+      <div>
+        <h2>{weather.name}</h2>
+        <p>Temperature: {weather.main.temp} °C</p>
+        <p>Description: {weather.weather[0].description}</p>
+        <p>Humidity: {weather.main.humidity}%</p>
+        <p>Wind Speed: {weather.wind.speed} m/s</p>
+      </div>
+    )}
+  </div>
   );
 }
 
